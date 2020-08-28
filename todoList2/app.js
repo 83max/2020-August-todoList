9b2b006dbaca8e8ca636e4c687431d8b6327d13c
83max <60070454+83max@users.noreply.github.com>
2020-08-28 19:28:36 +0300

    Update app.js

--- todoList2/app.js
+++ todoList2/app.js
@@ -93,6 +93,10 @@ new Vue ({
             });
 
             this.$refs.inputRef.value = "";
+            
+            const myUrl = new URL ("https://2020-august-todo-list.vercel.app/");
+	        const myURLhash = myUrl.hash = Math.round(Math.random() * 100000000000000000000);
+	        window.location = myUrl + myURLhash
 
             localStorage.setItem("todoArrayShow", JSON.stringify(this.todoArrayShow))
         },
